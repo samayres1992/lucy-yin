199106831c277c5e334e320c248e880d85127983
Sam Ayres <samayres1992@gmail.com>
2015-02-14 18:34:49 +0000

    Update

---
+++ wp-content/themes/mystile/content.php
@@ -0,0 +1,61 @@
+	<section class="landing">
+		<span class="howls-castle">	
+			<span class="hello">
+				<img class="logo" src="/wp-content/uploads/2015/02/logo.png" />
+				<h1><span class="l">Lucy</span> <span class="y">Yin</span></h2>
+			</span>
+			<span class="contactme">
+				<i class="icon-download"></i>
+				<!--<i class="icon-envelope"></i>-->
+			</span>
+		</span>
+	</section>
+	<section class="portfolio">
+		<h1 id="mywork">My Work</h1>
+		<div class="grid">
+			<div class="row">
+				<div class="slot-0-1">
+					<img class="gallery-image" src="wp-content/uploads/2015/02/tumblr_niuyeaeCuQ1qb2exzo1_1280.jpg" alt="gallery" />
+					<span class="desc">A nice description.</span>
+				</div>
+				<div class="slot-2-3">
+					<img class="gallery-image" src="wp-content/uploads/2015/02/tumblr_ni8ypxQr191qb2exzo1_1280.png" alt="gallery" />
+					<span class="desc">A nice description.</span>
+				</div>
+				<div class="slot-4-5">
+					<img class="gallery-image" src="wp-content/uploads/2015/02/tumblr_ng8d55bpkR1qb2exzo1_1280.jpg" alt="gallery" />
+					<span class="desc">A nice description.</span>
+				</div>
+			</div>
+			<div class="row">
+				<div class="slot-0-1">
+					<img class="gallery-image" src="wp-content/uploads/2015/02/tumblr_new52exhuU1qb2exzo1_1280.jpg" alt="gallery" />
+					<span class="desc">A nice description.</span>
+				</div>
+				<div class="slot-2-3">
+					<img class="gallery-image" src="wp-content/uploads/2015/02/tumblr_n27nblI9Ik1qb2exzo1_1280.jpg" alt="gallery" />
+					<span class="desc">A nice description.</span>
+				</div>
+				<div class="slot-4-5">
+					<img class="gallery-image" src="wp-content/uploads/2015/02/tumblr_n9hmzxNAL51qb2exzo1_1280.jpg" alt="gallery" />
+					<span class="desc">A nice description.</span>
+				</div>
+			</div>
+			<div class="row">
+				<div class="slot-0-1">
+					<img class="gallery-image" src="wp-content/uploads/2015/02/tumblr_n1mk92lyR41qb2exzo1_1280.jpg" alt="gallery" />
+					<span class="desc">A nice description.</span>
+				</div>
+				<div class="slot-2-3">
+					<img class="gallery-image" src="wp-content/uploads/2015/02/tumblr_n0t8f4bHJY1riydbvo1_500.jpg" alt="gallery" />
+					<span class="desc">A nice description.</span>
+				</div>
+				<div class="slot-4-5">
+					<img class="gallery-image" src="wp-content/uploads/2015/02/tumblr_mcm6bdEbrl1qz7t0xo1_1280.jpg" alt="gallery" />
+					<span class="desc">A nice description.</span>
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
+<?php get_footer(); ?>
---
+++ wp-content/themes/mystile/footer.php
@@ -0,0 +1,26 @@
+<footer>
+    <div class="creds">
+        <i class="icon-heart heart"></i>
+        <span class="lucy">A gift for Lucy,</span>
+        <span class="sam">with love from <a href="http://samayres.net">Sam</a>.</span>
+    </div>
+    <div class="social">
+        <!-- your social media here -->
+        <a href="#" class="social-icon">
+            <img src="/wp-content/themes/mystile/images/facebook.png" alt="facebook" />
+        </a>
+        <a href="#" class="social-icon">
+            <img src="/wp-content/themes/mystile/images/tumblr.png" alt="tumblr" />
+        </a>
+        <a href="#" class="social-icon">
+            <img src="/wp-content/themes/mystile/images/twitter.png" alt="twitter" />
+        </a>
+        <a href="#" class="social-icon">
+            <img src="/wp-content/themes/mystile/images/instagram.png" alt="instagram" />
+        </a>
+    </div>
+</footer>
+
+
+<script type="text/javascript" src="/wp-includes/js/jquery-scrollto.js"></script>
+<script type="text/javascript" src="/wp-includes/js/scrollToPortfolio.js"></script>
